a6311e43577117ea450c37abcfc3e66fcf13ba60
nicolas <nicolas@795f2d49-7102-0410-9315-962b31f5c57f>
2005-09-22 11:29:24 +0000

    Changement des tailles des vignettes
    git-svn-id: file:///home/nicolas/svn/perso/luxbum/trunk@12 795f2d49-7102-0410-9315-962b31f5c57f

--- index.php
+++ index.php
@@ -80,8 +80,8 @@ switch (TEMPLATE_THEME) {
    case '2COL':
       define ('LIMIT_THUMB_PAGE', 6);
       define ('NB_COL', 2);
-      define ('IMG_W', 300);
-      define ('IMG_H', 300);
+      define ('IMG_W', 125);
+      define ('IMG_H', 125);
       $template = 'vignette_2col.mxt';
       break;
 
