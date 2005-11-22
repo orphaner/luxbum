a2bedfa00e65916462891e3e49292afd5d5d32e3
nicolas <nicolas@795f2d49-7102-0410-9315-962b31f5c57f>
2005-11-22 16:41:52 +0000

    Continuation d'objectisation
    git-svn-id: file:///home/nicolas/svn/perso/luxbum/trunk@41 795f2d49-7102-0410-9315-962b31f5c57f

--- _fonctions/mysql.inc.php
+++ _fonctions/mysql.inc.php
@@ -5,10 +5,12 @@ class mysqlInc {
    var $db_link;
    var $nb_sql;
    var $mysqlserveur, $mysqlloggin, $mysqlpassword, $mysqlmaindb;
+   var $result;
 
    function mysqlInc ($mysqlserveur, $mysqlloggin, $mysqlpassword, $mysqlmaindb) {
       $this->nb_sql=0;
       $this->db_link = null;
+      $this->result = null;
       $this->mysqlserveur = $mysqlserveur;
       $this->mysqlloggin = $mysqlloggin;
       $this->mysqlpassword = $mysqlpassword;
@@ -27,25 +29,25 @@ class mysqlInc {
    // FONCTION POUR FAIRE UNE REQUETE
    function DbQuery ($query) {
       $this->nb_sql++;
-      $res = mysql_query ($query, $this->db_link) 
+      $this->result = mysql_query ($query, $this->db_link) 
          or die ('<br /><strong>ERREUR</strong> '.(mysql_error()).'<br /><strong>Requete</strong>: '.$query); 
-      return $res;
+      return $this->result;
    }
 
    // FONCTION POUR COMPTER LE NOMBRE D'ENREGISTREMENTS
    function DbCount ($query) {
-      $result = $this->DbQuery ($query);
-      return mysql_result ($result,0,"COUNT(*)");
+      $this->result = $this->DbQuery ($query);
+      return mysql_result ($this->result,0,"COUNT(*)");
    }
 
    // FONCTION POUR AFFICHER LE RESULTAT SUIVANT
-   function DbNextRow ($result) {
-      return mysql_fetch_array($result);
+   function DbNextRow () {
+      return mysql_fetch_array ($this->result);
    }
 
    // FONCTION POUR COMPTER LE NOMBRE DE RESULTATS D'UNE REQUETE
-   function DbNumRows ($result) {
-      return mysql_num_rows($result);
+   function DbNumRows () {
+      return mysql_num_rows ($this->result);
    }
 
    // FONCTION POUR FERMER LA CONNEXION AU SERVEUR
