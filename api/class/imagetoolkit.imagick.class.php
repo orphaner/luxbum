89082d3ca11578d2032c285d179837a8038253df
nicolas <nicolas@795f2d49-7102-0410-9315-962b31f5c57f>
2007-01-05 23:05:04 +0000

    refactoring de fichiers
    git-svn-id: file:///home/nicolas/svn/perso/luxbum/trunk@120 795f2d49-7102-0410-9315-962b31f5c57f

--- api/class/imagetoolkit.imagick.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ImageToolkitImagick extends imagetoolkit {
-   function createThumb($img_dest) {
-      exec('convert -size '.$this->imageDestWidth.'x'.$this->imageDestHeight.' "'.$this->image .'" -geometry '.$this->imageDestWidth.'x'.$this->imageDestHeight.' "'.$img_dest.'"');
-   }
-  }
-
-?>
